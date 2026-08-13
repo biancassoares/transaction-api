da8d72890823b5e31d230acebbf894c9b0f6514a
Bianca <bianca.soares0704@gmail.com>
2026-08-12 22:58:45 -0300

    add API documentation with Swagger

--- src/main/java/com/biancasoares/transactionapi/controller/TransactionController.java
+++ src/main/java/com/biancasoares/transactionapi/controller/TransactionController.java
@@ -4,6 +4,7 @@ import com.biancasoares.transactionapi.dto.TransactionRequest;
 import com.biancasoares.transactionapi.dto.TransactionResponse;
 import com.biancasoares.transactionapi.entity.Transaction; //pq recebe/ devolve transactions
 import com.biancasoares.transactionapi.service.TransactionService;// pq vai chamar o service
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus; //usado pra retornar status do http
 import org.springframework.web.bind.annotation.*; //importa varias anotacoes do spring
@@ -21,20 +22,38 @@ public class TransactionController { //declara o controller
         this.service = service;
     }
 
+    @Operation(
+            summary = "Get all transactions",
+            description = "Returns all registered transactions"
+    )
     @GetMapping //GET /transactions
     public List<TransactionResponse> getAll() { //endpoint retorna 1 lista
         return  service.findAll();
     }
 
+    @Operation(
+            summary = "Get transaction by ID",
+            description = "Returns a transaction by its ID"
+    )
     @GetMapping("/{id}")
     public TransactionResponse getById(@PathVariable Long id){
         return  service.findById(id);
     }
+
+    @Operation(
+            summary = "Create a transaction",
+            description = "Creates a new transaction"
+    )
+
     @PostMapping //POST /transactions
     public TransactionResponse create(@Valid @RequestBody TransactionRequest request) { // entra transactionrequest e retorno um transaction
         return service.save(request); //entrega p/ service salvar
     }
 
+    @Operation(
+            summary = "Update a transaction",
+            description = "Updates an existing transaction by its ID"
+    )
     @PutMapping("/{id}") //PUT /transaction {id}
     public TransactionResponse update( //metodo pra atualizar
             @PathVariable Long id, //anotacao q pega o id q veio da url
@@ -43,6 +62,10 @@ public class TransactionController { //declara o controller
         return service.update(id, request); //entrega os dois p/ o service
     }
 
+    @Operation(
+            summary = "Delete a transaction",
+            description = "Deletes a transaction by its ID"
+    )
     @DeleteMapping("/{id}") //DELETE /transactions/{id}
     @ResponseStatus(HttpStatus.NO_CONTENT) //feito a exclusao retorna NO CONTENT
     public void delete(@PathVariable Long id) { //pega o id da url
